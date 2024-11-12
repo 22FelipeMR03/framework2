d61c2536f9513e4f7c07a3351a555d35134e240a
22FelipeMR03 <160284016+22FelipeMR03@users.noreply.github.com>
2024-11-12 14:33:47 -0300

    Add files via upload

--- src/pages/produtos.js
+++ src/pages/produtos.js
@@ -1,42 +1,44 @@
-import { Container } from "react-bootstrap";
-import Menu from "./components/navbar"
+import { Container, Table } from "react-bootstrap";
 import Footer from "./components/footer";
+import Menu from "./components/navbar";
+
 
 export default function Produtos() {
     return <>
-        <Menu/>
+        <Menu />
         <Container>
             <h1>Produtos</h1>
-            <table class="table table-dark table-striped">
+            <Table striped bordered hover variant="dark">
                 <thead>
                     <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Username</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
                     </tr>
                     <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
+                        <td>2</td>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
                     </tr>
                     <tr>
-                    <th scope="row">3</th>
-                    <td colspan="2">Larry the Bird</td>
-                    <td>@twitter</td>
+                        <td>3</td>
+                        <td colSpan={2}>Larry the Bird</td>
+                        <td>@twitter</td>
                     </tr>
                 </tbody>
-            </table>
+            </Table>
         </Container>
-        <Footer/>
+        <Footer />
     </>
+
 }
