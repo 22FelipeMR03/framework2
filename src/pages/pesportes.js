5a26481cef906a3244cfacc64841e106a0fe847f
22FelipeMR03 <160284016+22FelipeMR03@users.noreply.github.com>
2024-12-10 17:51:36 +0000

    conceerto

--- src/pages/pesportes.js
+++ src/pages/pesportes.js
@@ -28,7 +28,7 @@ export default function Pesportes({produtos}) {
     </>
 }
 export async function getServerSideProps() {
-    const res = await fetch('http://localhost:3000/api/produtos/data')
+    const res = await fetch('https://framework2-git-main-22felipemr03s-projects.vercel.app/api/produtos/data')
     const repo = await res.json()
     return { props: { produtos: repo } }
 }
